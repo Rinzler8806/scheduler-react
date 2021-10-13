cfaaf91ad13682dde32e3145c81dd0fff1bc1cd3
Rinzler8806 <shane.rogers916@gmail.com>
2021-10-12 20:59:12 -0700

    added timeline buttons for day and week

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { createRef } from 'react';
 import FullCalendar from '@fullcalendar/react'; // must go before plugins
 import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
 import timeGridPlugin from '@fullcalendar/timegrid';
+import resourceTimelinePlugin from '@fullcalendar/resource-timeline';
 
 export default class DemoApp extends React.Component {
   render() {
@@ -11,9 +12,11 @@ export default class DemoApp extends React.Component {
     return (
       <FullCalendar
       ref={calendarRef}
-        plugins={[ dayGridPlugin, timeGridPlugin ]}
+      schedulerLicenseKey='CC-Attribution-NonCommercial-NoDerivatives'
+        plugins={[ dayGridPlugin, timeGridPlugin, resourceTimelinePlugin ]}
         initialView="dayGridMonth"
         events="https://fullcalendar.io/demo-events.json?start=2021-03-22&end=2021-08-22"
+        resources="https://fullcalendar.io/demo-resources.json?with-nesting&with-colors"
         customButtons={{
           myTimeDayBtn: {
             text: "timeDay",
@@ -41,11 +44,37 @@ export default class DemoApp extends React.Component {
               }
             }
           },
+          myTimeLineDayBtn: {
+            text: "timeLineDay",
+            click() {
+              
+              const calendar = calendarRef.current;
+
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+
+                calendarApi.changeView("resourceTimelineDay");
+              }
+            },
+          },
+          myTimeLineWeekBtn: {
+            text: "timeLineWeek",
+            click() {
+
+              const calendar = calendarRef.current;
+
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+
+                calendarApi.changeView("resourceTimelineWeek");
+              }
+            }
+          },
         }}
         headerToolbar={{
           left: "prev,next",
           center: "title",
-          right: "today,dayGridDay,dayGridWeek,dayGridMonth,myTimeDayBtn,myTimeWeekBtn",
+          right: "today,dayGridDay,dayGridWeek,dayGridMonth,myTimeDayBtn,myTimeWeekBtn,myTimeLineDayBtn,myTimeLineWeekBtn",
         }}
       />
     );
