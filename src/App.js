52cc4bdf336af5a3cdd10dfb1a70d72df0c09df2
Rinzler8806 <shane.rogers916@gmail.com>
2021-10-12 20:37:22 -0700

    timeDay and timeWeek buttons/views added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,53 @@
-import React from 'react'
-import FullCalendar from '@fullcalendar/react' // must go before plugins
-import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
+import React, { createRef } from 'react';
+import FullCalendar from '@fullcalendar/react'; // must go before plugins
+import dayGridPlugin from '@fullcalendar/daygrid'; // a plugin!
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 export default class DemoApp extends React.Component {
   render() {
+
+    const calendarRef = createRef()
+
     return (
       <FullCalendar
-        plugins={[ dayGridPlugin ]}
+      ref={calendarRef}
+        plugins={[ dayGridPlugin, timeGridPlugin ]}
         initialView="dayGridMonth"
+        events="https://fullcalendar.io/demo-events.json?start=2021-03-22&end=2021-08-22"
+        customButtons={{
+          myTimeDayBtn: {
+            text: "timeDay",
+            click() {
+              
+              const calendar = calendarRef.current;
+
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+
+                calendarApi.changeView("timeGridDay");
+              }
+            },
+          },
+          myTimeWeekBtn: {
+            text: "timeWeek",
+            click() {
+
+              const calendar = calendarRef.current;
+
+              if (calendar) {
+                const calendarApi = calendar.getApi();
+
+                calendarApi.changeView("timeGridWeek");
+              }
+            }
+          },
+        }}
+        headerToolbar={{
+          left: "prev,next",
+          center: "title",
+          right: "today,dayGridDay,dayGridWeek,dayGridMonth,myTimeDayBtn,myTimeWeekBtn",
+        }}
       />
-    )
+    );
   }
 }
